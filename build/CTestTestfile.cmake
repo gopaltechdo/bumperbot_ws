6e2e94963b5dd3655ecf2748333d698ba684a0ac
gopal kumar <gopalsingh.g60@gmail.com>
2024-08-05 15:48:19 +0530

    urdf

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/gopal/ros/bumperbot_ws/src
+# Build directory: /home/gopal/ros/bumperbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bumperbot_description")
+subdirs("bumperbot_examples")
---
+++ build/bumperbot_examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gopal/ros/bumperbot_ws/src/bumperbot_examples
+# Build directory: /home/gopal/ros/bumperbot_ws/build/bumperbot_examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
